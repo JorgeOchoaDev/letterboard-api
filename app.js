bbb1d075396aa01dbba451cd72c2b92610971644
JorgeOchoaDev <jl91ob@protonmail.com>
2019-11-09 14:44:30 -0600

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const http = require('http')
 
+const cors = require('cors')
+
 const express =require('express')
 
 const app = express()
 
+app.use(cors())
+
 app.get('/',(req,res)=>{
     res.send('get function working')
 })
+
 
 app.listen(3000, ()=>{console.log('listening on port 3000')})
