c5c95652a93e4b1183f8c1a683637894e5612714
JorgeOchoaDev <jl91ob@protonmail.com>
2019-11-09 14:40:17 -0600

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express =require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('<h1>get function working</h1>')
+    res.send('get function working')
 })
 
 app.listen(3000, ()=>{console.log('listening on port 3000')})
