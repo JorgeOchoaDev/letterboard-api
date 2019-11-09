420a1d0c1e8c369a3b2b40e2bbfdb687358e18a1
JorgeOchoaDev <jl91ob@protonmail.com>
2019-11-09 16:37:22 -0600

    ver 0.1

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const http = require('http')
-
 const cors = require('cors')
-
 const express =require('express')
-
 const app = express()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
 app.use(cors())
+
+
+mongoose.connect('mongodb+srv://jorge:Md2^qqPhw4!39CXDJ0F&@letterboard-tlnpa.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("Successfully connected to the database");
+    }).catch(err => {
+        console.log('Could not connect to the database. Exiting now...', err);
+        process.exit();
+    });
+
+
+const Movie = mongoose.model('Movie',{name:String})
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('get function working')
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body.name,)
+    const release = new Movie ({
+        name: req.body.name
+    })
+    release.save()
+    .then(()=>res.send('movie saved'))
+
 })
 
 
